23464c74d362266ee22ba290773083c0caeac4ff
Nehemiah <nmonrose@emagine.lc>
2023-11-18 22:02:23 -0400

    update files

---
+++ slackapi.py
@@ -0,0 +1,29 @@
+# Create a Python Script that will send a slack message to Group1_D9 once our Jenkins file has completed.
+# Python Module requests to interact with Slack API
+import os
+import requests
+from dotenv import load_dotenv, dotenv_values
+
+# call the .env in this script
+load_dotenv()
+
+# Send message as JSON
+# Send a Post request to the Slack Webhook
+
+# Use os.environ to hide the webhook url
+url = os.getenv('SLACK_WEBHOOK_URL')
+
+# apply secuirty measures
+# if url is present carry out the response
+if url:
+    message = {'text': 'Jenkins file has completed successfully!'}
+    
+    response = requests.post(url, json=message)
+
+
+    if response.status_code == 200:
+        print("Response is successful!")
+    else:
+        print(f"Error Alert! Status Code: {response.status_code}")
+else:
+    print("SLACK_WEBHOOK_URL environment variable is not set.")
